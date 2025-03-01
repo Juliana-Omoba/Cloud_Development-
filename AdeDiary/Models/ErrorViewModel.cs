b61c90750b5f19db385a771a3f1a9c830b00f40b
Juliana-Omoba <adeolaa33@gmail.com>
2025-03-01 17:36:33 +0200

    initial submission

---
+++ AdeDiary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdeDiary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
